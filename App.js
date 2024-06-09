a3a0487762e98e8d33122211242f5436b697f05b
Charity Foote <footefamily@yahoo.com>
2024-06-09 17:18:07 -0500

    Clean and working code

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 import { useState } from 'react';
-import { Text, View, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList, StyleSheet, Alert } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-//https://www.freepik.com/search?format=search&last_filter=type&last_value=icon&query=vintage+hand+drawn+icons&type=icon
 
 export default function App() {
-  const [task, setTask] = useState('');
+
+  const [currentTask, setCurrentTask] = useState({});
   const [tasks, setTasks] = useState([]);
   const [editIndex, setEditIndex] = useState(-1);
 
   const handleAddTask = () => {
-    if (task) {
+    if (currentTask.name) {
       if (editIndex !== -1) {
         const updatedTasks = [...tasks];
-        updatedTasks[editIndex] = task;
+        updatedTasks[editIndex] = currentTask;
         setTasks(updatedTasks);
         setEditIndex(-1);
+      } else if (tasks.some((task) => task.name === currentTask.name)) {
+        Alert.alert(
+          'Rename Task',
+          'You already have a task by this name, ' + currentTask.name + '. Please rename your task.',
+          [{
+            text: 'OK',
+            style: 'cancel'
+          }],
+          { cancelable: false }
+        )
       } else {
-        setTasks([...tasks, task]);
+        setTasks([...tasks, currentTask]);
       }
-      setTask('');
+      setCurrentTask({ name: "" });
     }
+  };
+
+  const handleIconClick = (index) => {
+    tasks[index].clicked = !tasks[index].clicked;
+    setTasks([...tasks]);
+  };
+
+  const handleDropDown = (index) => {
+    tasks[index].expanded = !tasks[index].expanded;
+    setTasks([...tasks]);
+  };
+
+  const handleAddNotes = (index, text) => {
+    tasks[index].notes = text;
+    setTasks([...tasks]);
   };
 
   const handleEditTask = (index) => {
     const taskToEdit = tasks[index];
-    setTask(taskToEdit);
+    setCurrentTask(taskToEdit);
     setEditIndex(index);
   };
 
@@ -35,17 +60,27 @@ export default function App() {
   };
 
   const renderItem = ({ item, index }) => (
-    <View style={styles.task}>
-      <Feather name="check-circle" color="black" />
-      <Text style={styles.itemList}>{item}</Text>
-      <View style={styles.taskButtons}>
-        <TouchableOpacity onPress={() => handleEditTask(index)}>
-          <Text style={styles.editButton}>Edit</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleDeleteTask(index)}>
-          <Text style={styles.deleteButton}>Delete</Text>
-        </TouchableOpacity>
+    <View>
+      <View style={styles.task}>
+        <Feather name={item.clicked ? "check-circle" : "circle"} style={styles.icon} onPress={() => handleIconClick(index)} />
+        <Text style={item.clicked ? styles.strikeThroughItem : styles.itemList}>{item.name}</Text>
+        <View style={styles.taskButtons}>
+          <TouchableOpacity onPress={() => handleDropDown(index)}>
+            <Feather name={'plus'} style={styles.addNotesButton} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleEditTask(index)}>
+            <Text style={styles.editButton}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+            <Text style={styles.deleteButton}>Delete</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      {item.expanded && (
+        <View>
+          <TextInput style={styles.notesBox} placeholder='Add notes' value={item.notes} onChangeText={(text) => handleAddNotes(index, text)} />
+        </View>
+      )}
     </View>
   );
 
@@ -53,13 +88,14 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.heading}>"Find out who you are and do it on purpose." -Dolly Parton</Text>
       <Text style={styles.title}>To Do List</Text>
-      <TextInput style={styles.input} placeholder='Enter Task' value={task} onChangeText={(text) => setTask(text)} />
+      <TextInput style={styles.input} placeholder='Enter Task' value={currentTask.name} onChangeText={(text) => setCurrentTask({ ...currentTask, name: text })} />
       <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
         <Text style={styles.addButtonText}>{editIndex !== -1 ? 'Update Task' : 'Add Task'}</Text>
       </TouchableOpacity>
       <FlatList data={tasks} renderItem={renderItem} keyExtractor={(item, index) => index.toString()} />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -100,6 +136,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18
   },
+  icon: {
+    fontSize: 19
+  },
   task: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -110,8 +149,29 @@ const styles = StyleSheet.create({
   itemList: {
     fontSize: 19
   },
+  strikeThroughItem: {
+    fontSize: 19,
+    textDecorationLine: 'line-through'
+  },
   taskButtons: {
     flexDirection: 'row'
+  },
+  addNotesButton: {
+    marginRight: 10,
+    color: '#b0ccb5',
+    fontWeight: 'bold',
+    fontSize: 18,
+    flex: 2
+  },
+  notesBox: {
+    borderWidth: 2,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 14,
+    marginHorizontal: 20,
+    overflow: 'wrap'
   },
   editButton: {
     marginRight: 10,
